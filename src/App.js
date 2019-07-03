f3c38cd5c135d2beb4bf1488d4fb25f3b6e2ee15
avoltis <v.agolli@live.com>
2019-07-03 23:57:02 +0200

    app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import Header from './components/Header';
 
 const client = new ApolloClient({
   uri: 'https://cleanmultiservices-graphql.herokuapp.com'
@@ -12,20 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
       </div>
     </ApolloProvider>
   );
